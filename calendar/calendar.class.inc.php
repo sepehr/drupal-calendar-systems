0c0bfa2d33cfa0873505f374e4782151bec1204e
Sepehr Lajevardi <me@sepehr.ws>
2011-04-11 13:10:55 +0430

    Ran through boombatower code formatter.

--- calendar/calendar.class.inc.php
+++ calendar/calendar.class.inc.php
@@ -1,18 +1,20 @@
 <?php
+
 /**
-* @todo
-* 	- esfahbod multi calendar should be used as of base of this package
-*/
+ * @todo
+ *   Esfahbod multi calendar should be used as of base of this package.
+ */
 
 class cmfcCalendar {
-	function factory($name,$options) {
-		if ($name=='old') {
-			//require_once('ca.class.inc.php');
-			//return new cmfcEmailSenderOld($options);
-		}
-		if ($name=='v1') {
-			require_once(dirname(__FILE__).'/v1/calendarV1.class.inc.php');
-			return cmfcCalendarV1::factory($options);
-		}
-	}
+  function factory($name, $options) {
+    switch ($name) {
+      // case 'old':
+        // require_once('ca.class.inc.php');
+        // return new cmfcEmailSenderOld($options);
+      case 'v1';
+        require_once dirname(__FILE__) . '/v1/calendarV1.class.inc.php';
+        return cmfcCalendarV1::factory($options);
+    }
+  }
 }
+
